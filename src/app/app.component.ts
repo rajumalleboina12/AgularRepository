b63dbad77f21a2249ccfae1ce6dffd24e66d6427
Raju Malleboina <raju.malleboina@proclink.local>
2022-11-07 18:01:19 +0530

    my-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Employee } from '../employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-angular';
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'raju',
+      country: 'India',
+      address: 'hyderabad',
+    },
+    {
+      id: 2,
+      name: 'Sharath',
+      country: 'India',
+      address: 'Delhi',
+    },
+    {
+      id: 3,
+      name: 'Avinash',
+      country: 'India',
+      address: 'Banglore',
+    },
+  ];
 }
